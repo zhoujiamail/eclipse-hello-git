193e82dc2226f3c9ee929933c4e2b4d75c6ef4a7
zhoujia2 <zhoujia2@chezhibao.com>
2019-07-03 14:16:15 +0800

    t3

--- eclipse-hello-git/src/main/java/com/study/git/eclipse_hello_git/App.java
+++ eclipse-hello-git/src/main/java/com/study/git/eclipse_hello_git/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Git World t2!" );
+        System.out.println( "Hello Git World t3!" );
     }
 }
